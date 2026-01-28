58227d102f36b76b848c95c16eee3248d02ac17d
HVR2511 <harsh.rokade25@gmail.com>
2026-01-28 22:30:42 +0530

    Add routing and page skeletons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BookDetails from "./pages/BookDetails";
+import EditBook from "./pages/EditBook";
+import Navbar from "./components/Navbar";
+import AddBook from "./components/AddBook";
 
 function App() {
 
   return (
-   <h1>Hello</h1>
+   <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addbook" element={<AddBook />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/edit/:id" element={<EditBook />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
